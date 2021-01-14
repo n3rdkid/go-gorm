66ee965c053fdfa24d177de347ebeb9e2b86687c
Saurav Adhikari <sauravads123@gmail.com>
2021-01-14 20:13:50 +0545

    improv : fixed added auto migrate

--- main.go
+++ main.go
@@ -17,9 +17,10 @@ func main() {
 	utils.LoadEnv()
 	dsn := fmt.Sprintf("%s:%s@%s/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&models.Vehicle{})
 	bike := models.Vehicle{TypeOfVehicle: "2 Wheeler", NoOfWheels: 2}
-	db.Create(&bike)
-	log.Printf("Connection Successful")
+	result := db.Create(&bike)
+	log.Printf("Insert Successful :%v", result)
 	if err != nil {
 		panic(err.Error())
 	}
